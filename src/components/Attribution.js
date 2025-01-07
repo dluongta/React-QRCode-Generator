9a50318d69f3c314afbd9ef8e549100edd67ec60
21020776TaDinhLuong <21020776@vnu.edu.vn>
2025-01-07 09:12:24 +0700

    complete app

---
+++ src/components/Attribution.js
@@ -0,0 +1,7 @@
+export default function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://react-app-cv-page.vercel.app/" target="_blank" rel="noreferrer">Luong Deepmind</a></p>
+      </footer>
+   );
+}
